d8127a1e6d42994445515d768b2b115b870f1953
Sahara-Abdi <47831982+sahara-Abdi@users.noreply.github.com>
2023-01-09 10:35:22 -0700

    Initial Commit

---
+++ sahara_prework/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sahara_prework
+//
+//  Created by Sahara Farah Abdi on 1/9/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
