ba6b3baf53689394b81d4351f620a62d46c748e6
Sahara-Abdi <47831982+sahara-Abdi@users.noreply.github.com>
2023-01-09 21:21:53 -0700

    submitting prework

---
+++ README.md
@@ -0,0 +1,27 @@
+## INTRODUCTION APP
+
+### App Description
+
+This is a simple IOS app that allows the user to input information to introduce themselves. They can input their name, current school, current level of education and the number of pets they own.
+
+### App Walk-though
+
+
+
+<img src="https://i.imgur.com/RlAv2OQ.gif" width=200><br> 
+
+`TODO://` In the Features section below, add an `x` between the `-[ ]` like this `- [x]` for any features you complete. (☝️ Remove this paragraph after after checking off user stories)
+
+### Required Features
+
+- [x] 1. App displays an image of a school's logo
+- [x] 2. App has three textfields for first, last, and school names
+- [x] 3. App has a segmented control that changes student year
+- [x] 4. Number of pet matches label is increased/decreased by stepper
+- [x] 5. Switch makes a statement about wanting more pets or not(true/false) 
+- [x] 6. Introduce yourself button shows alert box with an introduciton and dismiss button
+
+### Optional Features
+
+- [x] 1. I added education level
+- [x] 3. I also customized the text color to ASU's colors
--- sahara_prework/ViewController.swift
+++ sahara_prework/ViewController.swift
@@ -8,7 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var morePetsStepper: UIStepper!
+    @IBOutlet weak var morePetsSwitch: UISwitch!
+    @IBOutlet weak var educationLevelTextField: UITextField!
+    @IBAction func introduceSelfDidTapped(_ sender: UIButton) {
+        let year = yearSegmentedControl.titleForSegment(at: yearSegmentedControl.selectedSegmentIndex)
+        
+        // Creating a constant of type string that holds an introduction. The introduction receives the values from the outlet connections.
+let introduction = "My name is \(firstNameTextField.text!) \(lastNameTextField.text!) and I attend \(schoolNameTextField.text!).I am a \(educationLevelTextField.text!) student currently in my \(year!) year. I own \(numberOfPetsLabel.text!) pets.  It is \(morePetsSwitch.isOn) that I want more pets."
+// Creates the alert where we pass in our message, which our introduction.
+let alertController = UIAlertController(title: "My Introduction", message: introduction, preferredStyle: .alert)
+// A way to dismiss the box once it pops up
+let action = UIAlertAction(title: "Nice to meet you!", style: .default, handler: nil)
+// Passing this action to the alert controller so it can be dismissed
+        alertController.addAction(action)
+present(alertController, animated: true, completion: nil)
+        
+        
+    }
+    
+    @IBAction func stepperDidChange(_ sender: UIStepper) {
+        numberOfPetsLabel.text = "\(Int(sender.value))"
+    }
+    @IBOutlet weak var yearSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var numberOfPetsLabel: UILabel!
+    @IBOutlet weak var schoolNameTextField: UITextField!
+    @IBOutlet weak var lastNameTextField: UITextField!
+    @IBOutlet weak var firstNameTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
